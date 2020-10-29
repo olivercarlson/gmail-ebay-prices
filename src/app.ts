f2a8bdbfc800413c2b987809254ad9748a28c064
Oliver Carlson <oswain@gmail.com>
2020-10-28 17:35:32 -0700

    re-arranged file structure to include /src for ts.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,32 @@
 import fs from 'fs';
 import readline from 'readline';
 import { google } from 'googleapis';
+import { promisify } from 'util';
+import { getAllJSDocTagsOfKind } from 'typescript';
+import { oauth2 } from 'googleapis/build/src/apis/oauth2';
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const TOKEN_PATH = 'token.json';
 
-// TODO: refactor to async/await
+// const readFile = promisify(fs.readFile);
+// const writeFile = promisify(fs.writeFile);
 
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+interface Credentials {
+	installed: {
+		client_secret?: string;
+		client_id?: string;
+		redirect_uris: string | '';
+	};
+}
+
+// Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
 	if (err) return console.log('Error loading client secret file:', err);
 	// Authorize a client with credentials, then call the Gmail API.
@@ -20,7 +40,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-const authorize = (credentials, callback) => {
+function authorize(credentials: Credentials, callback) {
 	const { client_secret, client_id, redirect_uris } = credentials.installed;
 	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
@@ -30,7 +50,7 @@ const authorize = (credentials, callback) => {
 		oAuth2Client.setCredentials(JSON.parse(token.toString()));
 		callback(oAuth2Client);
 	});
-};
+}
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -38,7 +58,7 @@ const authorize = (credentials, callback) => {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-const getNewToken = (oAuth2Client, callback) => {
+function getNewToken(oAuth2Client, callback) {
 	const authUrl = oAuth2Client.generateAuthUrl({
 		access_type: 'offline',
 		scope: SCOPES,
@@ -61,12 +81,14 @@ const getNewToken = (oAuth2Client, callback) => {
 			callback(oAuth2Client);
 		});
 	});
-};
+}
+
 /**
  * Lists the labels in the user's account.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
+
 const listMessages = async (auth) => {
 	const gmail = google.gmail({ version: 'v1', auth });
 	let data: string | undefined;
@@ -101,34 +123,3 @@ const listMessages = async (auth) => {
 		}
 	);
 };
-
-// const listLabels = (auth) => {
-// 	const gmail = google.gmail({ version: 'v1', auth });
-// 	gmail.users.labels.list(
-// 		{
-// 			userId: 'me',
-// 		},
-// 		(err, res) => {
-// 			if (err) return console.log('The API returned an error: ' + err);
-// 			const labels = res?.data.labels;
-// 			if (labels?.length) {
-// 				console.log('Labels:');
-// 				labels.forEach((label) => {
-// 					console.log(`- ${label.name}`);
-// 				});
-// 			} else {
-// 				console.log('No labels found.');
-// 			}
-// 		}
-// 	);
-// };
-
-// ### BOILER PLATE ###
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-// 	res.send({ res: 'Hello World' });
-// });
-
-// app.listen(port, () => console.log(`listening on PORT ${port}`));
