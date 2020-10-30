dc6e1f3ed11c4e43c0a86c75d885d6a815e50ce8
Oliver Carlson <oswain@gmail.com>
2020-10-29 20:12:54 -0700

    fixed async/await logic for gmail auth

--- src/googleAuth.ts
+++ src/googleAuth.ts
@@ -5,18 +5,16 @@ import { google } from 'googleapis';
 import { promisify } from 'util';
 import { getAllJSDocTagsOfKind } from 'typescript';
 
+readline.Interface.prototype.question[promisify.custom] = function (prompt) {
+	return new Promise((resolve) => readline.Interface.prototype.question.call(this, prompt, resolve));
+};
+readline.Interface.prototype.questionAsync = promisify(readline.Interface.prototype.question);
+
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const TOKEN_PATH = 'token.json';
 
 const readFile = promisify(fs.readFile);
-// const writeFile = promisify(fs.writeFile);
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
+const writeFile = promisify(fs.writeFile);
 
 interface Credentials {
 	installed: {
@@ -26,16 +24,21 @@ interface Credentials {
 	};
 }
 
-// const  = await readFile('credentials.json');
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials  Path to the authorization client credentials.
+ * @returns {google.auth.OAuth2} Authorized OAuth 2 client
+ */
 
-export const getAuthorizedClient = async (credentialsPath) => {
-	let clientSecrets: Buffer;
+export const getAuthorizedClient = async (credentialsPath: string) => {
+	let clientSecrets: string;
 	try {
-		clientSecrets = await readFile(credentialsPath);
+		clientSecrets = await readFile(credentialsPath, { encoding: 'utf-8' });
 	} catch (e) {
 		return console.log('Error loading client secret file:', e);
 	}
-	const authorizedClient = await authorize(JSON.parse(clientSecrets.toString()));
+	const authorizedClient = await authorize(JSON.parse(clientSecrets));
 	return authorizedClient;
 };
 
@@ -43,7 +46,6 @@ export const getAuthorizedClient = async (credentialsPath) => {
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
  */
 
 // return an oAuth2Client
@@ -52,11 +54,12 @@ const authorize = async (credentials: Credentials) => {
 	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
 	// check for a previously stored token:
-	let token;
+	let token: string;
 	try {
-		token = await readFile(TOKEN_PATH);
+		token = await readFile(TOKEN_PATH, { encoding: 'utf-8' });
 	} catch (e) {
-		token = await getNewToken(oAuth2Client);
+		await getNewToken(oAuth2Client);
+		token = await readFile(TOKEN_PATH, { encoding: 'utf-8' });
 	}
 	oAuth2Client.setCredentials(JSON.parse(token.toString()));
 	return oAuth2Client;
@@ -66,29 +69,62 @@ const authorize = async (credentials: Credentials) => {
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
+ * @returns {google.auth.OAuth2} Authorized oAuth2Client and
  */
+// const getNewToken = async (oAuth2Client) => {
+// 	const authUrl = oAuth2Client.generateAuthUrl({
+// 		access_type: 'offline',
+// 		scope: SCOPES,
+// 	});
+// 	console.log('Authorize this app by visiting this url:', authUrl);
+// 	const rl = readline.createInterface({
+// 		input: process.stdin,
+// 		output: process.stdout,
+// 	});
+// 	const code = await rl.questionAsync('Enter the code from that page here: ');
+// 	rl.close();
+// 	return oAuth2Client.getToken(code, (err, token) => {
+// 		console.log(`token in here:`);
+// 		if (err) return console.error('Error retrieving access token', err);
+// 		oAuth2Client.setCredentials(token);
+// 		// Store the token to disk for later program executions
+// 		fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+// 			if (err) return console.error(err);
+// 			console.log('Token stored to', TOKEN_PATH);
+// 		});
+// 		return oAuth2Client;
+// 	});
+// };
+
 const getNewToken = async (oAuth2Client) => {
 	const authUrl = oAuth2Client.generateAuthUrl({
 		access_type: 'offline',
 		scope: SCOPES,
 	});
-	console.log('Authorize this app by visiting this url:', authUrl);
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout,
 	});
-	return rl.question('Enter the code from that page here: ', (code) => {
-		rl.close();
-		return oAuth2Client.getToken(code, (err, token) => {
-			if (err) return console.error('Error retrieving access token', err);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-				if (err) return console.error(err);
-				console.log('Token stored to', TOKEN_PATH);
-			});
-			return oAuth2Client;
-		});
-	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const code = await rl.questionAsync('Enter the code from that page here: ');
+	// console.log(`code is ${code}`);
+	rl.close();
+	let token: object;
+	try {
+		let res = await oAuth2Client.getToken(code);
+		token = res['tokens'];
+		oAuth2Client.setCredentials(token);
+		await writeFile(TOKEN_PATH, JSON.stringify(token));
+		console.log('Token stored to', TOKEN_PATH);
+		// return oAuth2Client;
+	} catch (err) {
+		return console.error(`Error retrieving access token ${err}`);
+	}
+	return oAuth2Client;
+
+	// Store the token to disk for later program executions
+	try {
+	} catch (err) {
+		return console.error(err);
+	}
 };
---
+++ src/types/readline-augmentation.d.ts
@@ -0,0 +1,5 @@
+declare module 'readline' {
+	export interface Interface {
+		questionAsync;
+	}
+}
